30317d188f0c17146c160797d2d20c34166f20c8
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-13 11:42:00 -0700

    Add files via upload

---
+++ helloworldreview.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello World Review!";
+
+    int a = 5;
+    int b = 10;
+
+    cout << "a = " << a;
+    cout << "b = " << b;
+
+    int sum = a + b;
+
+    cout << "sum = " << sum;
+
+    return 0;
+}
