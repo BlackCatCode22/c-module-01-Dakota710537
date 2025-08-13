d07728ea6e8c851d9c8c68c2fb32c52ba1ae180b
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-13 12:24:59 -0700

    Add files via upload

---
+++ addTwoNubers.cpp
@@ -0,0 +1,20 @@
+// Create by Dakota F on 8/13/2024
+//addTwoNumbers.cpp
+#include <iostream>
+using namespace std;
+
+// Create a very important function named main
+// This is where the program starts
+int main() {
+
+    // create variables needed for this program
+    int sumOfNums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << "Enter a number: ";
+    num1 = cin.get();
+    cout<<"Enter another number: ";
+    sumOfNums = num1 + num2;
+
+    return 0;
+}
