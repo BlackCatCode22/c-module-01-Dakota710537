ee85dfd493cffbd3f8d3c7682624d4c0dfe651d2
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-18 11:10:03 -0700

    Add files via upload

---
+++ addTwoInts.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+
+    using namespace std;
+
+    int main() {
+        int num1;
+        cout << "Please enter a number: ";
+        cin >> num1;
+        cout <<"Your number is: "<< num1;
+
+
+        return 0;
+    }
