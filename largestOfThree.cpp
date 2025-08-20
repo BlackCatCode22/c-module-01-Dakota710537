4162143c0c7d58ca74973d39557b0df5056f8eb8
Dakota710537 <dakotafernandez1025@gmail.com>
2025-08-20 11:42:15 -0700

    Add files via upload

--- largestOfThree.cpp
+++ largestOfThree.cpp
@@ -9,7 +9,6 @@ int main() {
     int num1;
     int num2;
     int num3;
-    int sumOfTheInts;
 
     //Initialize all my variables
     //Assign a zero value to variables
@@ -19,26 +18,26 @@ int main() {
     //Get num1
     cout <<"\n Please enter a value for num1:";
     cin >> num1;
-    cout <<"\n You entered " << num1 << "for num1";
+    cout <<"\n You entered " << num1 << " for num1";
 
     cout <<"\n Please enter a value for num2:";
     cin >> num2;
-    cout <<"\n You entered " << num2 << "for num2";
+    cout <<"\n You entered " << num2 << " for num2";
 
     cout <<"\n Please enter a value for num3:";
     cin >> num3;
-    cout <<"\n You entered " << num3 << "for num3";
+    cout <<"\n You entered " << num3 << " for num3";
 
     if (num1 > num2) {
         if (num1 > num3) {
-            cout <<"\n The Greatest of the three is" << num1;
+            cout <<"\n The Greatest of the three is " << num1;
         }else {
-            cout <<"\n The Greatest of the three is" << num3;
+            cout <<"\n The Greatest of the three is " << num3;
         }
     }else if (num2 > num3) {
-        cout <<"\n The Greatest of the three is" << num2;
+        cout <<"\n The Greatest of the three is " << num2;
     }else {
-        cout <<"\n The Greatest of the three is" << num3;
+        cout <<"\n The Greatest of the three is " << num3;
     }
 
 
